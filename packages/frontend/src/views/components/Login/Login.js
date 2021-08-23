748732d133f5b5cf60cf5a22782f6bdea160d6a6
Tanu Grover <v-tanugrover@microsoft.com>
2021-08-23 20:13:36 +0530

    updates

--- packages/frontend/src/views/components/Login/Login.js
+++ packages/frontend/src/views/components/Login/Login.js
@@ -133,6 +133,7 @@ export default function SignInSide() {
               color="primary"
               loading={isSubmitting}
               className={classes.submit}
+              
             >
             SIGN IN
             </Button>
