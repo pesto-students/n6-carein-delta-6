dcc85b3819b17b2730911b2dd0da22f76b9d5667
Vinit Borole <vinitbarole@gmail.com>
2021-08-30 02:22:00 +0530

    s3 file upload

--- packages/backend/config/plugin.js
+++ packages/backend/config/plugin.js
@@ -1,18 +1,30 @@
 module.exports = ({ env }) => ({
   email: {
-    provider: "nodemailer",
+    provider: 'amazon-ses',
     providerOptions: {
-      host: "email-smtp.us-east-2.amazonaws.com",
-      port: 465,
-      auth: {
-        user: "AKIAYT43VJ6HEBNC2PPS",
-        pass: "BCNd88xdDshvr0y7ahTiVHuC1MP36PFyIk7i6C/Q6IQ6",
-      },
-      secure: true,
+      key: env('AWS_ACCESS_KEY_ID'),
+      secret: env('AWS_ACCESS_SECRET'),
+      amazon: 'https://email.us-east-2.amazonaws.com',
     },
     settings: {
-      defaultFrom: "vinitbarole@gmail.com",
-      defaultReplyTo: "carein.pvt@gmail.com",
+      defaultFrom: 'vinitbarole@gmail.com',
+      defaultReplyTo: 'carein.pvt@gmail.com',
+    },
+  },
+
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET'),
+      },
     },
   },
 });
+// SMTP Username:
+// AKIAYT43VJ6HC5AWT254
+// SMTP Password:
+// BF0M9elE8N+48kq23aqbjk41RfAbR03pF1TTjNWEhpL/
