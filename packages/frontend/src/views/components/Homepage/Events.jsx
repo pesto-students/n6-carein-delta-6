84c1c03fcbd57f924067b9986d2b7f23dc7616b0
shubham91 <shubhambhatia@91social.com>
2021-09-03 01:12:41 +0530

    integration of events done

--- packages/frontend/src/views/components/Homepage/Events.jsx
+++ packages/frontend/src/views/components/Homepage/Events.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import SideNav from "../../common/SideNav";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import WalkCard from "../../common/WalkCard";
 import DashboardLayout from "../../../containers/TheLayout";
- 
+import { useDispatch,useSelector } from 'react-redux';
+import { listEvents  } from '../../../actions/eventActions';
+
+
 const Events = () => {
+  const eventData = useSelector((state) => state.apiRes)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listEvents())
+  },[]);
+  console.log(eventData)
   
-  let data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   return (
     <DashboardLayout rightDrawer={true}>
-      {data.map((text, index) => (
-        <WalkCard key={index} />
+      {eventData.map((text, index) => (
+        <WalkCard event={text} key={index} />
       ))}
     </DashboardLayout>
   );
