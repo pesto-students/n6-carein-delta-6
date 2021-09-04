74a0b72dc2c0f62e154726dcdd85345e222a1202
shubham91 <shubhambhatia@91social.com>
2021-09-04 22:49:32 +0530

    event update

--- packages/frontend/src/views/components/Homepage/Events.jsx
+++ packages/frontend/src/views/components/Homepage/Events.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
-import SideNav from "../../common/SideNav";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
 import WalkCard from "../../common/WalkCard";
-import DashboardLayout from "../../../containers/TheLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { listEvents } from "../../../actions/eventActions";
 
