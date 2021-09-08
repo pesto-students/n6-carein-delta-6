cd6746d6f5c60d8eaec111f7d7063f4a22e0e7ca
Tanu Grover <v-tanugrover@microsoft.com>
2021-09-08 14:50:21 +0530

    updates

--- packages/frontend/src/views/common/AssistanceServiceCard.jsx
+++ packages/frontend/src/views/common/AssistanceServiceCard.jsx
@@ -1,69 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
+import CardActions from "@material-ui/core/CardActions";
 import { Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 250,
-    maxWidth: 250,
-    minHeight: 250,
-    margin: theme.spacing(0, 40, 2),
+    [theme.breakpoints.down("sm")]: {
+      width: "100% !important",
+    },
+    width: "calc(100% - 10px) !important",
+    margin: "10px 5px !important",
   },
   card: {
-    margin: "5px 5px ",
+    margin: "5px 5px",
   },
   text: {
-    margin: theme.spacing(-2, 0, 0),
+    margin: theme.spacing(0, 0, 0),
+    fontSize: "14px",
   },
   main: {
-    margin: theme.spacing(0, 0, 2),
+    margin: theme.spacing(0, 0, 1),
+    fontSize: "18px",
   },
-  buttonstyle: {
-    margin: theme.spacing(3, 0, 2),
+  buttoninterested: {
+    margin: theme.spacing(0, 0, 0),
   },
-  removebutton: {
-    margin: theme.spacing(3, 3, 2),
+  buttonregister: {
+    margin: theme.spacing(0, 0, 0),
   },
 }));
 
-const AssistanceServiceCard = () => {
+
+
+function AssistanceServiceCard(props) {
   const classes = useStyles();
-  var service= {};
+
   return (
-    <Grid xs={12} md={4} lg={3} sm={6}>
+    <Grid item xs={12} md={4} lg={4} sm={6}>
       <Card className={classes.card}>
-        <CardMedia component="img" image="./assets/media/bg/assistance.jpg" />
-        <CardContent>
-          <Typography
-            className={classes.main}
-            gutterBottom
-            variant="h5"
-            component="h6"
-          >
-            {service.name}
-          </Typography>
-          <Button
-            size="small"
-            className={classes.buttonstyle}
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/ServiceDetails"
-          >
-            More Details
-          </Button>
-        </CardContent>
+        <CardActionArea>
+          <CardMedia component="img" image="./assets/media/bg/assistance.jpg" />
+          <CardContent>
+            <Typography
+              className={classes.main}
+              gutterBottom
+              variant="h5"
+              component="h6"
+            >
+            
+             Assistance
+            </Typography>
+            <Typography
+              className={classes.text}
+              variant="p"
+              color="textSecondary"
+              component="p"
+            >
+              
+              24*7 Custom Assistance 
+            </Typography>
+            
+            <Typography
+              className={classes.text}
+              variant="p"
+              color="textSecondary"
+              component="p"
+            >
+             
+              Rs. 499/day
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              className={classes.buttoninterested}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/ServiceDetails">
+              More Details
+            </Button>
+          </CardActions>
+        </CardActionArea>
       </Card>
     </Grid>
   );
-};
+}
 
 export default AssistanceServiceCard;
